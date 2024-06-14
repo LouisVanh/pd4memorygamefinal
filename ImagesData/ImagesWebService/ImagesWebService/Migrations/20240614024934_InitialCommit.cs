d1860ba17b1c5984cf39ecc34fe7874b1e56fa3a
LouisVanhove <vanhovelouis123@gmail.com>
2024-06-14 04:52:27 +0200

    Migration added

---
+++ ImagesData/ImagesWebService/ImagesWebService/Migrations/20240614024934_InitialCommit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ImagesWebService.Migrations
+{
+    public partial class InitialCommit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // FIRST MIGRATION COMMENTED STUFF OUT, SO ITS A FULL BACKUP
+
+            //migrationBuilder.CreateTable(
+            //    name: "Images",
+            //    columns: table => new
+            //    {
+            //        Id = table.Column<int>(type: "int", nullable: false),
+            //        Name = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+            //        IsBack = table.Column<bool>(type: "bit", nullable: true),
+            //        Image = table.Column<byte[]>(type: "varbinary(max)", nullable: false)
+            //    },
+            //    constraints: table =>
+            //    {
+            //    });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.DropTable(
+            //    name: "Images");
+        }
+    }
+}
